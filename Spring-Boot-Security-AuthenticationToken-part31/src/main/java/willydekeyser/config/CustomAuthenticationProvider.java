1015ece11f0b84bd65bb6fb8dcb7e6ce076f8e87
Willy De Keyser <spring.boot.programming@gmail.com>
2026-05-19 23:23:42 +0200

    Update

--- Spring-Boot-Security-AuthenticationToken-part31/src/main/java/willydekeyser/config/CustomAuthenticationProvider.java
+++ Spring-Boot-Security-AuthenticationToken-part31/src/main/java/willydekeyser/config/CustomAuthenticationProvider.java
@@ -1,13 +1,10 @@
 package willydekeyser.config;
 
-import java.util.List;
-
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,13 +12,11 @@ public class CustomAuthenticationProvider implements AuthenticationProvider{
 
 	@Override
 	public @Nullable Authentication authenticate(Authentication authentication) throws AuthenticationException {
-		IO.println("CustomAuthenticationProvider: " + authentication);
-		List<SimpleGrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_USER"),
-				new SimpleGrantedAuthority("ROLE_ADMIN"), 
-				new SimpleGrantedAuthority("ROLE_TEST"));
-		UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken("user", "", authorities);
+		IO.println("CustomAuthenticationProvider: " + authentication);	
+		CustomAuthenticationToken usernamePasswordAuthenticationToken = new CustomAuthenticationToken();
 		IO.println("CustomAuthenticationProvider: " + usernamePasswordAuthenticationToken);
 		return usernamePasswordAuthenticationToken;
+		
 	}
 
 	@Override
