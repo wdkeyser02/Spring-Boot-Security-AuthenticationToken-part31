465c2deca842c25b7d7bfc0b67dc766212633e0b
Willy De Keyser <spring.boot.programming@gmail.com>
2026-06-23 14:53:47 +0200

    Update controller

--- Spring-Boot-Security-AuthenticationToken-part31/src/main/java/willydekeyser/controller/HomeController.java
+++ Spring-Boot-Security-AuthenticationToken-part31/src/main/java/willydekeyser/controller/HomeController.java
@@ -16,16 +16,7 @@ public class HomeController {
         if (authentication != null) {
             userName = authentication.getName().toUpperCase();
         }
-        return ("""
-                <center>
-                <h1>Spring Boot Tutorial</h1>
-                <h2>Home Page!</h2>
-                <p>Username: %s</p>
-                <a href='/public'>Public</a><br/>
-                <a href='/user'>Private for User</a><br/>
-                <a href='/admin'>Private for Admin</a>
-                </center>
-                """).formatted(userName);
+        return ("HOME-Page! Username: %s").formatted(userName);
     }
 
     @GetMapping("/public")
@@ -33,38 +24,16 @@ public class HomeController {
         if (authentication != null) {
             userName = authentication.getName().toUpperCase();
         }
-        return ("""
-                <center>
-                <h1>Spring Boot Tutorial</h1>
-                <h2>Public Page!</h2>
-                <p>Username: %s</p>
-                <a href='/'>Home</a>
-                </center>
-                """).formatted(userName);
+        return ("Public-Page! Username: %s").formatted(userName);
     }
 
     @GetMapping("/user")
     public String private_page_user(Principal principal) {
-        return ("""
-                <center>
-                <h1>Spring Boot Tutorial</h1>
-                <h2>Private Page for User!</h2>
-                <p>Username: %s</p>
-                <a href='/'>Home</a>
-                </center>
-                """).formatted(principal.getName().toUpperCase());
+        return ("Private-Page-for-User! Username: %s").formatted(principal.getName().toUpperCase());
     }
 
     @GetMapping("/admin")
     public String private_page_admin(Authentication authentication) {
-        return ("""
-                <center>
-                <h1>Spring Boot Tutorial</h1>
-                <h2>Private Page for Admin!</h2>
-                <p>Username: %s</p>
-                <p>Roles: %s</p>
-                <a href='/'>Home</a>
-                </center>
-                """).formatted(authentication.getName().toUpperCase(), authentication.getAuthorities());
+        return ("Private-Page-for-Admin! Username: %s Roles: %s").formatted(authentication.getName().toUpperCase(), authentication.getAuthorities());
     }
 }
